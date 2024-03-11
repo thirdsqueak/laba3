ed5ac0671d7718a2e36a12bb606e8fe59176abc8
thirdsqueak <lvasilkov952@gmail.com>
2024-03-11 19:50:19 +0300

    add text

--- first.php
+++ first.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>test</title>
 </head>
 <body>
     <?php
-    echo "testchanges<br>/n"; 
+    echo "testchanges number 2<br>/n"; 
+    echo "HELP ME<br>/n"; 
     ?>
 </body>
 </html>
